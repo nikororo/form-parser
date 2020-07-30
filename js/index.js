9580021c1d0e2acd059bec35c4ce5f88aa962f60
nikororo <nikoro52@gmail.com>
2020-07-30 18:04:52 +0700

    fields+buttons

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
 const inputFile = document.getElementById("inputJsonFile");
 const form = document.getElementById("form");
 
+const inputType = {
+    file: "multiple",
+    email: "multiple"
+}
+
+
 function Parser(jsonFile) {
     let reader = new FileReader();
     let json;
+
     reader.readAsText(jsonFile);
     reader.onload = function() {
         json = reader.result;
@@ -15,8 +22,6 @@ function createTitle(content) {
     form.innerHTML = "";
     let title = document.createElement('h1');
     let name = content.name;
-
-    delete content.name;
 
     name = name[0].toUpperCase() + name.slice(1);
     name = name.replace(/_/g, " ");
@@ -33,42 +38,118 @@ function createForm(content) {
             case "fields":
                 createFields(content[a]);
                 break;
+            case "references":
+                createRefs(content[a]);
+                break;
+            case "buttons": 
+                createButtons(content[a]);
+                break;
         }
-        
     });
 }
 
 function createFields (fields) {
+    let id = 0;
 
     fields.map(obj => {
         let {label, input} = obj;
-
         let oneField;
+
         if (label) {
             oneField = document.createElement('label');
+            oneField.setAttribute('for', id);
             oneField.innerHTML = label;
             form.appendChild(oneField);
         }
+
         if (input) {
-            oneField = document.createElement('input');
-            
-            for (let attribute in input) {
-                
-                if (Array.isArray(input[attribute])) {
-                    oneField.setAttribute('list', attribute);
-                    createDataList(attribute, input[attribute]);
-
-                } else if (attribute === 'mask') {
-
-                    console.log('mask');
-                    
-                } else if (input[attribute] === true) oneField.setAttribute(attribute, '');
-                else oneField.setAttribute(`${attribute}`, input[attribute]);
-
-                form.appendChild(oneField);
-            }
+            oneField = createInput(input, id);
+            form.appendChild(oneField);
         }
+        id++;
     });
+}
+
+function createInput(input, id) {
+    if (input.multiple && !(inputType[input.type]==="multiple")) {
+        delete input.type;
+        return createSelect(input, id);
+    }  
+
+    if (input.type === "textarea") {
+        delete input.type;
+        return createTextarea(input, id);
+
+    } else if (input.type === "file") {
+        return createInputFile(input, id);
+    }
+
+    let inputField = document.createElement('input');
+    inputField.setAttribute('id', id);
+
+    for (let attribute in input) {
+        
+        if (Array.isArray(input[attribute])) {
+            inputField.setAttribute('list', attribute);
+            createDataList(attribute, input[attribute]);
+
+        } else if (attribute === 'mask') {
+            //накладывать маску
+            
+        } else if (input[attribute] === true) inputField.setAttribute(attribute, '');
+        else inputField.setAttribute(`${attribute}`, input[attribute]);
+    }
+    return inputField;
+
+}
+
+function createInputFile(input, id) {
+    let inputFileField = document.createElement('input');
+    inputFileField.setAttribute('id', id);
+
+    for (let attribute in input) {
+        
+        if (attribute === "filetype") {
+            let strAccept = "";
+            input[attribute].map(type => strAccept += "." + type + ", ");
+            inputFileField.setAttribute('accept', strAccept.slice(0, -2));
+
+        } else if (input[attribute] === true) inputFileField.setAttribute(attribute, '');
+        else inputFileField.setAttribute(`${attribute}`, input[attribute]);
+    }
+    return inputFileField;
+
+}
+
+function createSelect(input, id) {
+    let selectField = document.createElement('select');
+    selectField.setAttribute('id', id);
+
+    for (let attribute in input) {
+
+        if (Array.isArray(input[attribute])) {
+            input[attribute].map(opt => {
+                let option = document.createElement('option');
+                option.setAttribute('value', opt);
+                option.innerHTML = opt;
+                selectField.appendChild(option);
+            });
+
+        } else if (input[attribute] === true) selectField.setAttribute(attribute, '');
+        else selectField.setAttribute(`${attribute}`, input[attribute]);
+    }
+    return selectField;
+}
+
+function createTextarea(input, id) {
+    let textField = document.createElement('textarea');
+    textField.setAttribute('id', id);
+    for (let attribute in input) {
+        
+        if (input[attribute] === true) textField.setAttribute(attribute, '');
+        else textField.setAttribute(`${attribute}`, input[attribute]);
+    }
+    return textField;
 }
 
 function createDataList(attribute, options) {
@@ -83,6 +164,16 @@ function createDataList(attribute, options) {
     });
 }
 
+function createRefs(refs) {
+    //выводить ссылки
+}
 
+function createButtons(buttons) {
+    buttons.map(but => {
+        let buttonTag = document.createElement('button');
+        buttonTag.innerHTML = but.text;
+        form.appendChild(buttonTag);
+    });
+}
 
 inputFile.addEventListener("change", () => Parser(inputFile.files[0]));
